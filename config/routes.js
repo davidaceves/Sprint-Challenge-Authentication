012e578f37e14d8fe9564d13a3243116e315d1d4
David <digital0david@gmail.com>
2019-06-15 13:17:44 -0400

    register/route-model

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('../auth/authenticate');
+const secrets = require('./secrets.js')
+const Users = require('./route-model.js')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,9 +13,38 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1h"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
 function register(req, res) {
   // implement user registration
-}
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = generateToken(saved)
+
+      res.status(201).json({
+        message: `Welcome ${saved.username}!`,
+        authToken: token
+      });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+
+};
 
 function login(req, res) {
   // implement user login
